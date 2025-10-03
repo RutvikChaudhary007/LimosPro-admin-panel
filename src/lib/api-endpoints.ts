2d161f53b4bcd7df228267e56fe4d4d75c9aa775
RutvikChaudhary007 <rutvik.chaudhary@qalbit.com>
2025-10-03 11:29:46 +0530

    fix: prod endpoint

--- src/lib/api-endpoints.ts
+++ src/lib/api-endpoints.ts
@@ -3,9 +3,9 @@
 //  export const BASE_URL1  = import.meta.env.VITE_API_BASE_URL1 ||'http://localhost:3001/api/v1';
 //  export const BASE_URL2  = import.meta.env.VITE_API_BASE_URL2 ||'http://localhost:3002/api/v1';
 
- export const BASE_URL  = 'http://192.168.1.7:3000/api/v1';
- export const BASE_URL1  = import.meta.env.VITE_API_BASE_URL1 ||'http://192.168.1.7:3001/api/v1';
- export const BASE_URL2  = import.meta.env.VITE_API_BASE_URL2 ||'http://192.168.1.7:3002/api/v1';
+ export const BASE_URL  = import.meta.env.VITE_API_BASE_URL ||'http://apilimo.qalbit.in/user-service/';
+ export const BASE_URL1  = import.meta.env.VITE_API_BASE_URL1 ||'http://apilimo.qalbit.in/booking-service/';
+ export const BASE_URL2  = import.meta.env.VITE_API_BASE_URL2 ||'http://apilimo.qalbit.in/admin-service/';
 
 //  export const BASE_URL  = import.meta.env.VITE_API_BASE_URL ||'http://192.168.1.10:3000/api/v1';
 //  export const BASE_URL1  = import.meta.env.VITE_API_BASE_URL1 ||'http://192.168.1.10:3001/api/v1';
