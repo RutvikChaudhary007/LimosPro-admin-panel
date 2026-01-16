8c462aef44631c120f9582ebe8530eab28c0c970
RutvikChaudhary007 <rutvik.chaudhary@qalbit.com>
2026-01-16 13:58:13 +0530

    fix: remove unused import

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -5,7 +5,7 @@ import {
   useQueryClient,
 } from "@tanstack/react-query";
 import type { AxiosError } from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
   blogService,
   bulkDeleteChauffeur,
