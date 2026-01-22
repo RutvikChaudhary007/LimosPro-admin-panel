927fcd09ea573b5e25264c606b425678cf1030a4
Fahim Shaikh <fahim.shaikh@qalbit.com>
2026-01-22 19:53:11 +0530

    fix: bugs in admin panel

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -304,7 +304,7 @@ export const routesConfig = [
   {
     layout: "protected",
     module: "chauffeur",
-    permission: "manageChauffeurs",
+    permission: ["manageChauffeurs", "managePartnerChauffeurs"],
     action: "view",
     routes: [
       { path: constant.ROUTING_URLS.CHAUFFEUR, element: ChauffeurPage },
